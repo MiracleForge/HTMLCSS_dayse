5f44ceeae93744c8624ff4f980ab3eb80f390ca9
MiracleForge <paulomoreirarosado@hotmail.com>
2023-09-25 15:29:04 -0300

    update gulpfiles.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,37 @@
-const { src, dest, series } = require('gulp');
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
-function buildSite() {
-  // Tarefa para compilar e preparar seu site, por exemplo, minificar CSS, JavaScript, etc.
-  return src('src/**/*')
-    .pipe(dest('dist/'));
+// Função para compilar SCSS em CSS
+function scssTask() {
+  return src('src/scss/styles.scss')
+    .pipe(sass())
+    .pipe(dest('dist/css'))
+    .pipe(browserSync.stream());
 }
 
-exports.build = series(buildSite);
+// Função para processar arquivos JavaScript
+function jsTask() {
+  return src('src/js/*.js')
+    .pipe(dest('dist/js'))
+    .pipe(browserSync.stream());
+}
+
+// Função para iniciar o servidor de desenvolvimento com BrowserSync
+function browserSyncServe() {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+}
+
+// Função para observar alterações nos arquivos SCSS e JavaScript
+function watchTask() {
+  watch('src/scss/**/*.scss', scssTask);
+  watch('src/js/*.js', jsTask);
+  watch('*.html').on('change', browserSync.reload);
+}
+
+exports.default = series(scssTask, jsTask, browserSyncServe, watchTask);
+exports.build = series(scssTask, jsTask);
