aa1b432015035824cdf1042aca322b354c3c36a0
MiracleForge <paulomoreirarosado@hotmail.com>
2023-10-06 01:11:35 -0300

    Update inicio.js
    Bug not fixed
    Module type eresed

--- pages/Inicio/inicio.js
+++ pages/Inicio/inicio.js
@@ -37,7 +37,6 @@ export default () => {
 
     // Inclua um elemento de script para carregar o main.js
     const script = document.createElement('script');
-    script.type = 'module';
     script.src = './js/main.js'; // Substitua pelo caminho correto para o main.js
     document.body.appendChild(script);
 
